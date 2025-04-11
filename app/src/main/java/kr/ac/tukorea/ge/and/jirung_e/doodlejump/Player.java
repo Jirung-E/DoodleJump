d0081e2f9a2ef5278f903c00c8d23facba3a84d8
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2025-04-12 02:00:18 +0900

    플레이어 점프시키기 - 보이는 게임화면의 세로길이가 16m로 취급되므로 중력이 약해보인다. - 아래가 +y방향이라 계산시에 주의해야한다.

--- app/src/main/java/kr/ac/tukorea/ge/and/jirung_e/doodlejump/Player.java
+++ app/src/main/java/kr/ac/tukorea/ge/and/jirung_e/doodlejump/Player.java
@@ -10,16 +10,16 @@ public class Player {
     private static final float HEIGHT_HALF = 1.0f;
     private float x, y;
     private float dx, dy;
-    private static final float GRAVITY = -9.8f;
+    private static final float GRAVITY = 9.8f;
+    private static final float JUMP_SPEED = -10.0f;
 
     public Player() {
         x = GameView.SCREEN_WIDTH / 2;
         y = GameView.SCREEN_HEIGHT / 2;
         dx = 0;
-        dy = 0;
+        dy = JUMP_SPEED;
 
-        dstRect.set(x - WIDTH_HALF, y - HEIGHT_HALF,
-                x + WIDTH_HALF, y + HEIGHT_HALF);
+        updateRect();
     }
 
     private static Bitmap bitmap;
@@ -28,10 +28,23 @@ public class Player {
     }
 
     public void update() {
+        dy += GRAVITY * GameView.frameTime;
+        x += dx * GameView.frameTime;
+        y += dy * GameView.frameTime;
+        updateRect();
 
+        if(y > GameView.SCREEN_HEIGHT) {
+            y = GameView.SCREEN_HEIGHT;
+            dy = JUMP_SPEED;
+        }
     }
 
     public void draw(Canvas canvas) {
         canvas.drawBitmap(bitmap, null, dstRect, null);
+    }
+
+    private void updateRect() {
+        dstRect.set(x - WIDTH_HALF, y - HEIGHT_HALF,
+                x + WIDTH_HALF, y + HEIGHT_HALF);
     }
 }
